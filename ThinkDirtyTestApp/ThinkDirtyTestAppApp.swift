c736b91a9996cc214db9ddf0be721bdbf7fa4a43
Vivienne Fosh <lonkly@lanternapps.com>
2022-09-25 20:07:40 -0400

    Initial Commit

---
+++ ThinkDirtyTestApp/ThinkDirtyTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkDirtyTestAppApp.swift
+//  ThinkDirtyTestApp
+//
+//  Created by Vivienne Fosh on 25.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ThinkDirtyTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
